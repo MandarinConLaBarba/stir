7721327be4ead21089d053616aeb3a2050097e77
Mandarin Drummond <mandarind@gmail.com>
2012-08-17 21:40:31 -0400

    Added test stubs (generated by stir) and modified generator to look for stuff on module.exports..probably still will miss other expressions, like exports = module.exports = blah..

---
+++ test/generator.js
@@ -0,0 +1,55 @@
+var generator = require('../lib/generator.js'),
+should = require('should');
+
+describe("generator.js", function() {
+    
+        
+        describe("generate", function() {
+
+            it("should do something..", function(done){
+
+                //TODO: make this pass..
+                should.exist(undefined);
+
+            });
+
+        });
+    
+        
+        describe("writeTest", function() {
+
+            it("should do something..", function(done){
+
+                //TODO: make this pass..
+                should.exist(undefined);
+
+            });
+
+        });
+    
+        
+        describe("getMethodList", function() {
+
+            it("should do something..", function(done){
+
+                //TODO: make this pass..
+                should.exist(undefined);
+
+            });
+
+        });
+    
+        
+        describe("compileFileList", function() {
+
+            it("should do something..", function(done){
+
+                //TODO: make this pass..
+                should.exist(undefined);
+
+            });
+
+        });
+    
+
+});
