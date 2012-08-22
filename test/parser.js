4bd64e3a3dc98589e8d8c3e2de70159c7aaa9397
Mandarin Drummond <mandarind@gmail.com>
2012-08-21 20:19:25 -0400

    Fixed some tests

--- test/parser.js
+++ test/parser.js
@@ -20,40 +20,40 @@ describe("parser.js", function() {
             fs.readFileSync.restore();
         });
 
-//        it("should return one method if there is one method in exports", function(){
+        it("should return one method if there is one method in exports", function(){
+
+            var fakeModule = "exports.testFunction123 = function() {};";
+//            fakeModule += "module.exports = {func1 : function() {} };"
+//            fakeModule += "someVariable = {func2 : function() {}};"
 //
-//            var fakeModule = "exports.testFunction123 = function() {};";
-////            fakeModule += "module.exports = {func1 : function() {} };"
-////            fakeModule += "someVariable = {func2 : function() {}};"
-////
-////            fakeModule += "module.exports = function() {};";
-////            fakeModule += "module.exports = someVariable;"
-//
-//            fs.readFileSync.returns(fakeModule);
-//
-//            var functions = parser.getMethodList("some/Fake/Path");
-//
-//            should.equal(1, functions.length);
-//            functions[0].should.equal("testFunction123");
-//
-//        });
-//
-//        it("should return all the methods of an object assigned to module.exports", function() {
-//            var fakeModule = "var someVariable = {};";
-//            fakeModule += "\n";
-//            fakeModule += " someVariable.func1 = function() {};";
-//            fakeModule += "\n";
-//            fakeModule += " someVariable.func2 = function() {};";
-//            fakeModule += "\n";
-//            fakeModule += "module.exports = someVariable";
-//
-//            fs.readFileSync.returns(fakeModule);
-//
-//            var functions = parser.getMethodList("some/Fake/Path");
-//
-//            should.equal(2, functions.length);
-//
-//        });
+//            fakeModule += "module.exports = function() {};";
+//            fakeModule += "module.exports = someVariable;"
+
+            fs.readFileSync.returns(fakeModule);
+
+            var functions = parser.getMethodList("some/Fake/Path");
+
+            should.equal(1, functions.length);
+            functions[0].should.equal("testFunction123");
+
+        });
+
+        it("should return all the methods of an object assigned to module.exports", function() {
+            var fakeModule = "var someVariable = {};";
+            fakeModule += "\n";
+            fakeModule += " someVariable.func1 = function() {};";
+            fakeModule += "\n";
+            fakeModule += " someVariable.func2 = function() {};";
+            fakeModule += "\n";
+            fakeModule += "module.exports = someVariable";
+
+            fs.readFileSync.returns(fakeModule);
+
+            var functions = parser.getMethodList("some/Fake/Path");
+
+            should.equal(2, functions.length);
+
+        });
 
         it("should return all methods of an object variable assigned to module.exports", function() {
             var fakeModule = "var someVariable = { func1 : function() {}, func2 : function() {}};";
@@ -68,18 +68,18 @@ describe("parser.js", function() {
             should.equal(2, functions.length);
 
         });
-//
-//        it("should return all the methods of an object on the right side of a module.exports assignment", function(){
-//
-//            var fakeModule = "module.exports = {testFunction1 : function(){}, testFunction2 : function(){}}";
-//
-//            fs.readFileSync.returns(fakeModule);
-//
-//            var functions = parser.getMethodList("some/Fake/Path");
-//
-//            should.equal(2, functions.length);
-//
-//        });
+
+        it("should return all the methods of an object on the right side of a module.exports assignment", function(){
+
+            var fakeModule = "module.exports = {testFunction1 : function(){}, testFunction2 : function(){}}";
+
+            fs.readFileSync.returns(fakeModule);
+
+            var functions = parser.getMethodList("some/Fake/Path");
+
+            should.equal(2, functions.length);
+
+        });
 
     });
 
