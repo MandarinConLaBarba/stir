509c0990f8d2ad3f22415c6061be04d6993ea206
Mandarin Drummond <mandarind@gmail.com>
2012-08-28 17:56:16 -0400

    Added another sample

---
+++ test/sample/dir3/explicit.js
@@ -0,0 +1,41 @@
+var targetModule = require(''),
+    should = require('should');
+
+describe("test/sample/dir3/explicit", function() {
+    
+        
+
+        
+            describe("function7", function() {
+
+
+                it("should do something..", function(){
+
+                    //TODO: make this pass..
+                    should.exist(undefined);
+
+                });
+            });
+
+         
+
+    
+        
+
+        
+            describe("function8", function() {
+
+
+                it("should do something..", function(){
+
+                    //TODO: make this pass..
+                    should.exist(undefined);
+
+                });
+            });
+
+         
+
+    
+
+});
