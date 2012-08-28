4297b62402ff2d923264366734a58fe6d8f66b3f
Mandarin Drummond <mandarind@gmail.com>
2012-08-28 07:28:29 -0400

    Some cleanup

--- test/parser.js
+++ test/parser.js
@@ -1,98 +1,100 @@
-var parser = require('../lib/parser.js'),
+var Parser = require('../lib/parser.js'),
     fs = require('fs'),
     path = require('path'),
     sinon = require('sinon'),
-    should = require('should');
+    should = require('should'),
+    util = require('util');
 
 
-describe("parser.js", function() {
+describe("parser2.js", function() {
 
+    var readFileSyncStub;
 
-    describe("getMethodList", function() {
+    beforeEach(function() {
+        readFileSyncStub = sinon.stub(fs, 'readFileSync')
+    });
 
-        var readFileSyncStub;
+    afterEach(function() {
+        fs.readFileSync.restore();
+    });
 
-        beforeEach(function() {
-            readFileSyncStub = sinon.stub(fs, 'readFileSync')
-        });
+    it("should return one method if there is one method in exports", function(){
 
-        afterEach(function() {
-            fs.readFileSync.restore();
-        });
+        var fakeModule = "exports.testFunction123 = function() {};";
+        fs.readFileSync.returns(fakeModule);
 
-        it("should return one method if there is one method in exports", function(){
+        var parser = new Parser();
 
-            var fakeModule = "exports.testFunction123 = function() {};";
-//            fakeModule += "module.exports = {func1 : function() {} };"
-//            fakeModule += "someVariable = {func2 : function() {}};"
-//
-//            fakeModule += "module.exports = function() {};";
-//            fakeModule += "module.exports = someVariable;"
+        var parsed = parser.parse("some/Fake/Path");
 
-            fs.readFileSync.returns(fakeModule);
-
-            var functions = parser.getMethodList("some/Fake/Path");
-
-            should.equal(1, functions.length);
-            functions[0].should.equal("testFunction123");
-
-        });
-
-        it("should work with module.exports = function()", function() {
-            var fakeModule = "module.exports = function() {}";
-
-            fs.readFileSync.returns(fakeModule);
-
-            var functions = parser.getMethodList("some/Fake/Path");
-
-            should.equal(1, functions.length);
-
-        });
-
-        it("should return all the methods of an object assigned to module.exports", function() {
-            var fakeModule = "var someVariable = {};";
-            fakeModule += "\n";
-            fakeModule += " someVariable.func1 = function() {};";
-            fakeModule += "\n";
-            fakeModule += " someVariable.func2 = function() {};";
-            fakeModule += "\n";
-            fakeModule += "module.exports = someVariable";
-
-            fs.readFileSync.returns(fakeModule);
-
-            var functions = parser.getMethodList("some/Fake/Path");
-
-            should.equal(2, functions.length);
-
-        });
-
-        it("should return all methods of an object variable assigned to module.exports", function() {
-            var fakeModule = "var someVariable = { func1 : function() {}, func2 : function() {}};";
-            fakeModule += "\n";
-            fakeModule += "\n";
-            fakeModule += "module.exports = someVariable";
-
-            fs.readFileSync.returns(fakeModule);
-
-            var functions = parser.getMethodList("some/Fake/Path");
-
-            should.equal(2, functions.length);
-
-        });
-
-        it("should return all the methods of an object on the right side of a module.exports assignment", function(){
-
-            var fakeModule = "module.exports = {testFunction1 : function(){}, testFunction2 : function(){}}";
-
-            fs.readFileSync.returns(fakeModule);
-
-            var functions = parser.getMethodList("some/Fake/Path");
-
-            should.equal(2, functions.length);
-
-        });
+        should.equal(1, parsed.suggestedTests.length);
+        parsed.suggestedTests[0].should.equal("testFunction123");
 
     });
+
+    it("should work with module.exports = function()", function() {
+        var fakeModule = "module.exports = function() {}";
+
+        fs.readFileSync.returns(fakeModule);
+
+        var parser = new Parser();
+
+        var parsed = parser.parse("some/Fake/Path");
+
+        should.equal(1, parsed.suggestedTests.length);
+
+    });
+
+    it("should return all the methods of an object assigned to module.exports", function() {
+        var fakeModule = "var someVariable = {};";
+        fakeModule += "\n";
+        fakeModule += " someVariable.func1 = function() {};";
+        fakeModule += "\n";
+        fakeModule += " someVariable.func2 = function() {};";
+        fakeModule += "\n";
+        fakeModule += "module.exports = someVariable";
+
+        fs.readFileSync.returns(fakeModule);
+        var parser = new Parser();
+
+        var parsed = parser.parse("some/Fake/Path");
+
+        console.log(util.inspect(parsed, false, null));
+        should.equal(2, parsed.suggestedTests.length);
+
+    });
+
+    it("should return all methods of an object variable assigned to module.exports", function() {
+        var fakeModule = "var someVariable = { func1 : function() {}, func2 : function() {}};";
+        fakeModule += "\n";
+        fakeModule += "\n";
+        fakeModule += "module.exports = someVariable";
+
+        fs.readFileSync.returns(fakeModule);
+
+        var parser = new Parser();
+
+        var parsed = parser.parse("some/Fake/Path");
+
+        should.equal(2, parsed.suggestedTests.length);
+
+    });
+
+    it("should return all the methods of an object on the right side of a module.exports assignment", function(){
+
+        var fakeModule = "module.exports = {testFunction1 : function(){}, testFunction2 : function(){}}";
+
+        fs.readFileSync.returns(fakeModule);
+
+        var parser = new Parser();
+
+        var parsed = parser.parse("some/Fake/Path");
+
+        should.equal(2, parsed.suggestedTests.length);
+
+    });
+
+
 
 
 
